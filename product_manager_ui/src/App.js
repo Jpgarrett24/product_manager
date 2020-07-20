a44cac7ace8acf27e031023ae84920ce353474d2
jpgarrett <jpgarrett24@gmail.com>
2020-07-20 14:27:08 -0700

    Part 1 of product manager completed: User can now add product to database using a UI form with react.

--- product_manager_ui/src/App.js
+++ product_manager_ui/src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Link, Redirect, Router } from "@reach/router";
 import './App.css';
+import CreateProduct from './views/CreateProduct';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Link to="/products/create">Add Products</Link>
+      <Router>
+        <CreateProduct path="/products/create" />
+      </Router>
+    </>
   );
 }
 
